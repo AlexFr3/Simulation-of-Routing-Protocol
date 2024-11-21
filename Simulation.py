8f0854541d767d727be8b1503876be5eeffd5ab6
Alex Frisoni <alex.frisoni2@studio.unibo.it>
2024-11-21 14:46:13 +0100

    update algorithm

--- Simulation.py
+++ Simulation.py
@@ -1,42 +1,67 @@
-class Nodo:
-    def __init__(self, id_nodo):
-        self.id_nodo = id_nodo
-        self.vicini = {}  # Distanza ai nodi vicini
-        self.tabella_routing = {id_nodo: (0, id_nodo)}  # Distanza a se stesso
+class Node:
+    def __init__(self, name):
+        self.name = name
+        self.routing_table = {}  # Destinazione: (Costo, Next Hop)
+        self.neighbors = {}      # Vicini: Costo
 
-    def aggiungi_vicino(self, vicino, distanza):
-        self.vicini[vicino.id_nodo] = distanza
-        self.tabella_routing[vicino.id_nodo] = (distanza, vicino.id_nodo)
+    def update_table(self, neighbor_name, neighbor_table):
+        updated = False
+        for dest, (cost_to_dest, next_hop) in neighbor_table.items():
+            new_cost = self.neighbors[neighbor_name] + cost_to_dest
+            if dest not in self.routing_table or new_cost < self.routing_table[dest][0]:
+                self.routing_table[dest] = (new_cost, neighbor_name)
+                updated = True
+        return updated
 
-    def aggiorna_tabella(self, tabelle_vicine):
-        cambiato = False
-        for nodo_vicino, tabella in tabelle_vicine.items():
-            distanza_vicino = self.vicini[nodo_vicino]
-            for destinazione, (distanza, next_hop) in tabella.items():
-                nuova_distanza = distanza_vicino + distanza
-                if (destinazione not in self.tabella_routing or 
-                    nuova_distanza < self.tabella_routing[destinazione][0]):
-                    self.tabella_routing[destinazione] = (nuova_distanza, nodo_vicino)
-                    cambiato = True
-        return cambiato
+    def __str__(self):
+        return f"Node {self.name} Routing Table: {self.routing_table}"
 
-# Creazione dei nodi e della rete
-nodi = [Nodo(i) for i in range(5)]
-nodi[0].aggiungi_vicino(nodi[1], 1)
-nodi[1].aggiungi_vicino(nodi[2], 1)
-nodi[2].aggiungi_vicino(nodi[3], 1)
-nodi[3].aggiungi_vicino(nodi[4], 1)
-nodi[4].aggiungi_vicino(nodi[0], 1)  # Anello
 
-# Simulazione
-for step in range(10):  # Massimo 10 cicli
-    cambiato = False
-    for nodo in nodi:
-        tabelle_vicine = {vicino: nodi[vicino].tabella_routing for vicino in nodo.vicini}
-        if nodo.aggiorna_tabella(tabelle_vicine):
-            cambiato = True
-    if not cambiato:
-        break
-    print(f"Ciclo {step + 1}:")
-    for nodo in nodi:
-        print(f"Nodo {nodo.id_nodo} tabella: {nodo.tabella_routing}")
+class Network:
+    def __init__(self):
+        self.nodes = {}
+
+    def add_node(self, name):
+        self.nodes[name] = Node(name)
+
+    def add_link(self, node1, node2, cost):
+        self.nodes[node1].neighbors[node2] = cost
+        self.nodes[node2].neighbors[node1] = cost
+
+    def simulate_routing(self):
+        converged = False
+        iteration = 0
+        while not converged:
+            print(f"Iteration {iteration}")
+            converged = True
+            for node_name, node in self.nodes.items():
+                for neighbor_name in node.neighbors:
+                    if node.update_table(neighbor_name, self.nodes[neighbor_name].routing_table):
+                        converged = False
+            for node in self.nodes.values():
+                print(node)
+            iteration += 1
+            print("-" * 50)
+
+
+# Definizione della rete
+network = Network()
+network.add_node("A")
+network.add_node("B")
+network.add_node("C")
+network.add_node("D")
+
+network.add_link("A", "B", 1)
+network.add_link("A", "C", 5)
+network.add_link("B", "C", 2)
+network.add_link("B", "D", 4)
+network.add_link("C", "D", 1)
+
+# Inizializza le tabelle di routing
+for node in network.nodes.values():
+    for neighbor, cost in node.neighbors.items():
+        node.routing_table[neighbor] = (cost, neighbor)
+    node.routing_table[node.name] = (0, node.name)
+
+# Simula il protocollo
+network.simulate_routing()
